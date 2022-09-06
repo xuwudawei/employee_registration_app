b44e7163f723c74f73e083260ed551645053a0f5
xuwudawei <xuwudawei@outlook.com>
2022-09-06 13:20:13 +0530

    updated ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,30 +4,41 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'landing',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'loginscreen',
-    loadChildren: () => import('./loginscreen/loginscreen.module').then( m => m.LoginscreenPageModule)
+    loadChildren: () =>
+      import('./loginscreen/loginscreen.module').then(
+        (m) => m.LoginscreenPageModule
+      ),
   },
   {
     path: 'signupscreen',
-    loadChildren: () => import('./signupscreen/signupscreen.module').then( m => m.SignupscreenPageModule)
+    loadChildren: () =>
+      import('./signupscreen/signupscreen.module').then(
+        (m) => m.SignupscreenPageModule
+      ),
   },
 
   {
     path: 'landing',
-    loadChildren: () => import('./landing-page/landing-page.module').then( m => m.LandingPagePageModule)
+    loadChildren: () =>
+      import('./landing-page/landing-page.module').then(
+        (m) => m.LandingPagePageModule
+      ),
   },
 
   {
     path: 'employee',
-    loadChildren: () => import('./employee/employee.module').then( m => m.EmployeePageModule)
+    loadChildren: () =>
+      import('./employee/employee.module').then((m) => m.EmployeePageModule),
   },
 ];
 
