f2f0f12d86cc3be02162c9b7c8d75d658ea09d27
xuwudawei <xuwudawei@outlook.com>
2022-09-06 15:33:47 +0530

    validation for add new employee done

--- src/app/add-employee-page/add-employee-page.page.ts
+++ src/app/add-employee-page/add-employee-page.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-employee-page',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee-page.page.scss'],
 })
 export class AddEmployeePagePage implements OnInit {
+    addNewEmployeeForm: FormGroup = this.addNewEmployeeFormBuilder.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    ],
 
-  constructor() { }
+    uid: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        // Validators.pattern('^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$'),
+      ],
+    ],
+    designation: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    ],
+  });
 
-  ngOnInit() {
-  }
+  constructor(
+    public modalCtrl: ModalController,
+    public addNewEmployeeFormBuilder: FormBuilder
+  ) {}
 
+  ngOnInit() { }
+  submitEmployeeForm(){}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -10,8 +11,17 @@ import { OneSignal } from '@awesome-cordova-plugins/onesignal/ngx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },OneSignal],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(),
+    FormsModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    OneSignal,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
